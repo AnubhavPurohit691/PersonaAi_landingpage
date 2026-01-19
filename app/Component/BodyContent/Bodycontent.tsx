55c2fdb382c43ca331441ef9a9557897fb9116e0
AnubhavPurohit <anubhavpurohitno1@gmail.com>
2026-01-19 05:37:04 +0530

    fixing v1

--- app/Component/BodyContent/Bodycontent.tsx
+++ app/Component/BodyContent/Bodycontent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Bodycontent = () => {
   return (
-    <div className='flex justify-center text-center gap-4 items-center max-w-xl mx-auto py-5 flex-col px-4'>
+    <div className='flex justify-center text-center gap-4 items-center max-w-xl mx-auto py-10 flex-col px-4'>
      <h1 className='text-3xl md:text-4xl font-bold'>Everything Your Team Needs to Work Smarter</h1>  
      <div className='text-center'>from task tracking to real-time chat,our features we build to keep your team connected. organized and moving forward--together</div>
     </div>
